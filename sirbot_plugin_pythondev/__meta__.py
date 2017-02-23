616126d4a17ff76bbd11c2dc6a04437a7c426231
Ovv <ovv@outlook.com>
2017-02-23 21:26:50 +0100

    setup.py

---
+++ sirbot_plugin_pythondev/__meta__.py
@@ -0,0 +1,20 @@
+DATA = {
+    # Ordered by commit log entries
+    "author": ('mikefromit <mike.arbelaez@gmail.com>, '
+               'Matt Rasband <matt.rasband@gmail.com>, '
+               'Ovv <ovv@outlook.com>, '
+               'Nick Humrich <nick.humrich@gmail.com>, '
+               'Julioocz <julioocz@gmail.com>, '
+               'Shawn McElroy <shawn@skift.io>, '
+               'Sean Johnson <seandavidjohnson@gmail.com>'),
+    "author_email": 'pythondev.slack@gmail.com',
+    "copyright": 'Copyright 2016 Python Developers Community',
+    "description": 'Pythondev slack plugin for the good Sir Bot a lot',
+    "license": 'MIT',
+    "name": 'pythondev_sirbot',
+    "url": 'https://gitlab.com/PythonDevCommunity/sirbot-plugin-pythondev',
+    # Versions should comply with PEP440. For a discussion on
+    # single-sourcing the version across setup.py and the project code,
+    # see http://packaging.python.org/en/latest/tutorial.html#version
+    "version": '0.0.1',
+}
