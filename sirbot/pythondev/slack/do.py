387c2da96580f8bee8bed9b4a692bccdd72f9616
Joe Sanford <josephsanford@gmail.com>
2017-06-11 11:40:12 -0400

    Added Digital Ocean referral command

---
+++ sirbot/pythondev/slack/do.py
@@ -0,0 +1,17 @@
+DIGITAL_OCEAN_REFFERRAL_URL = 'https://m.do.co/c/457f0988c477'
+DIGITAL_OCEAN_URL = 'https://digitalocean.com'
+
+DIGITAL_OCEAN_TEXT = '''Here at Python Developers we host our website and
+Slack bot on <{}|Digital Ocean>. If you are planning on using Digital Ocean,
+please use our <{}|referral code>. You get 10 USD and help support the 
+community!'''.format(DIGITAL_OCEAN_URL, DIGITAL_OCEAN_REFFERRAL_URL)
+
+
+def add_to_slack(slack):
+    slack.add_command('/do', func=share_digital_ocean, public=False)
+
+
+async def share_digital_ocean(command, slack, facades):
+    response = command.response()
+    response.text = DIGITAL_OCEAN_TEXT
+    await slack.send(response)
