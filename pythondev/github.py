227a31ff0f15f287c3e81bf33245e7e006fd7c31
Quentin Dawans <contact@ovv.wtf>
2017-07-16 22:46:24 +0200

    update git webhook for gidgethub (#57)
    Add message for created release and
    created/deleted repo

--- pythondev/github.py
+++ pythondev/github.py
@@ -13,94 +13,142 @@ class GitHubEndpoint:
 
     def add(self):
         github = registry.get('github')
-        github.add_event(self.issues, 'issues')
-        github.add_event(self.pull_request, 'pull_request')
 
-    async def issues(self, event):
-        att = None
+        github.register(self.issues_opened, 'issues', action='opened')
+        github.register(self.issues_closed, 'issues', action='closed')
+        github.register(self.issues_reopened, 'issues', action='reopened')
 
-        if event['action'] == 'opened':
-            att = self._issue_format(event, 'good')
-        elif event['action'] == 'closed':
-            att = self._issue_format(event, 'danger')
+        github.register(self.pr_opened, 'pull_request', action='opened')
+        github.register(self.pr_closed, 'pull_request', action='closed')
+        github.register(self.pr_reopened, 'pull_request', action='reopened')
 
-        if att:
-            slack = registry.get('slack')
-            channel = await slack.channels.get(
-                name=self.config['github']['channel']
-            )
-            msg = SlackMessage(to=channel)
-            msg.attachments.append(att)
-            await slack.send(msg)
+        github.register(self.release_created, 'release', action='published')
+        github.register(self.repo_created, 'repository', action='created')
+        github.register(self.repo_deleted, 'repository', action='deleted')
 
-    def _issue_format(self, event, color):
+    async def issues_opened(self, event):
+        await self.send_issue_msg(event, 'good')
+
+    async def issues_closed(self, event):
+        await self.send_issue_msg(event, 'danger')
+
+    async def issues_reopened(self, event):
+        await self.send_issue_msg(event, 'good')
+
+    async def send_issue_msg(self, issue, color):
         att = Attachment(
-            fallback='issue {}'.format(event['action']),
+            fallback='issue {}'.format(issue.data['action']),
             color=color,
-            text=event['issue']['body'],
-            title='Issue {action} in <{repo_url}|{name}>:'
-                  ' <{url}|{title}>'.format(
-                repo_url=event['repository']['html_url'],
-                url=event['issue']['html_url'],
-                name=event['repository']['name'],
-                action=event['action'],
-                title=event['issue']['title']
+            text='*<{url}|{title}>*\n{body}'.format(
+                url=issue.data['issue']['html_url'],
+                title=issue.data['issue']['title'],
+                body=issue.data['issue']['body']
             ),
-            author_icon=event['sender']['avatar_url'],
-            author_name=event['sender']['login'],
-            author_link=event['sender']['html_url'],
+            title='Issue {action} in <{repo_url}|{name}>'.format(
+                repo_url=issue.data['repository']['html_url'],
+                name=issue.data['repository']['name'],
+                action=issue.data['action'],
+            ),
+            author_icon=issue.data['sender']['avatar_url'],
+            author_name=issue.data['sender']['login'],
+            author_link=issue.data['sender']['html_url'],
             footer=', '.join(
-                label['name'] for label in event['issue']['labels']
+                label['name'] for label in issue.data['issue']['labels']
             )
         )
 
-        return att
+        slack = registry.get('slack')
+        channel = await slack.channels.get(
+            name=self.config['github']['channel']
+        )
+        msg = SlackMessage(to=channel)
+        msg.attachments.append(att)
+        await slack.send(msg)
 
-    async def pull_request(self, event):
-        att = None
+    async def pr_opened(self, event):
+        await self.send_pr_msg(event, 'opened', 'good')
 
-        if event['action'] == 'opened':
-            data = {'color': 'good', 'action': 'opened'}
-            att = self._pull_request_format(event, data)
-        elif event['action'] == 'closed':
-            if event['pull_request']['merged']:
-                data = {'color': '#6f42c1', 'action': 'merged'}
-            else:
-                data = {'color': 'danger', 'action': 'closed'}
+    async def pr_closed(self, event):
+        if event.data['pull_request']['merged']:
+            await self.send_pr_msg(event, 'merged', '#6f42c1')
+        else:
+            await self.send_pr_msg(event, 'closed', 'danger')
 
-            att = self._pull_request_format(event, data)
+    async def pr_reopened(self, event):
+        await self.send_pr_msg(event, 'reopened', 'good')
 
-        if att:
-            slack = registry.get('slack')
-            channel = await slack.channels.get(
-                name=self.config['github']['channel']
-            )
-            msg = SlackMessage(to=channel)
-            msg.attachments.append(att)
-            await slack.send(msg)
-
-    def _pull_request_format(self, event, data):
+    async def send_pr_msg(self, pr, action, color):
         footer = '+ {add} / - {del_}'.format(
-            add=event['pull_request']['additions'],
-            del_=event['pull_request']['deletions']
+            add=pr.data['pull_request']['additions'],
+            del_=pr.data['pull_request']['deletions']
         )
 
         att = Attachment(
-            fallback='pull request {}'.format(data['action']),
+            fallback='pull request {}'.format(action),
             title='Pull request {action} in <{repo_url}|{name}>:'
                   ' <{url}|{title}>'.format(
-                repo_url=event['repository']['html_url'],
-                url=event['pull_request']['html_url'],
-                name=event['repository']['name'],
-                action=data['action'],
-                title=event['pull_request']['title'],
+                repo_url=pr.data['repository']['html_url'],
+                url=pr.data['pull_request']['html_url'],
+                name=pr.data['repository']['name'],
+                action=pr.data['action'],
+                title=pr.data['pull_request']['title'],
             ),
-            color=data['color'],
-            text=event['pull_request']['body'],
-            author_icon=event['sender']['avatar_url'],
-            author_name=event['sender']['login'],
-            author_link=event['sender']['html_url'],
+            color=color,
+            text='*<{url}|{title}>*\n{body}'.format(
+                url=pr.data['pull_request']['html_url'],
+                title=pr.data['pull_request']['title'],
+                body=pr.data['pull_request']['body']
+            ),
+            author_icon=pr.data['sender']['avatar_url'],
+            author_name=pr.data['sender']['login'],
+            author_link=pr.data['sender']['html_url'],
             footer=footer
         )
 
-        return att
+        slack = registry.get('slack')
+        channel = await slack.channels.get(
+            name=self.config['github']['channel']
+        )
+        msg = SlackMessage(to=channel)
+        msg.attachments.append(att)
+        await slack.send(msg)
+
+    async def release_created(self, event):
+
+        slack = registry.get('slack')
+        channel = await slack.channels.get(
+            name=self.config['github']['channel']
+        )
+        msg = SlackMessage(to=channel)
+        msg.text = 'Release {release} created in {repo} by {user}'.format(
+            release=event.data['release']['tag_name'],
+            repo=event.data['repository']['name'],
+            user=event.data['sender']['login']
+        )
+        await slack.send(msg)
+
+    async def repo_created(self, event):
+
+        slack = registry.get('slack')
+        channel = await slack.channels.get(
+            name=self.config['github']['channel']
+        )
+        msg = SlackMessage(to=channel)
+        msg.text = 'Repository {repo} created by {user} :tada:'.format(
+            repo=event.data['repository']['name'],
+            user=event.data['sender']['login']
+        )
+        await slack.send(msg)
+
+    async def repo_deleted(self, event):
+
+        slack = registry.get('slack')
+        channel = await slack.channels.get(
+            name=self.config['github']['channel']
+        )
+        msg = SlackMessage(to=channel)
+        msg.text = 'Repository {repo} deleted by {user} :cold_sweat:'.format(
+            repo=event.data['repository']['name'],
+            user=event.data['sender']['login']
+        )
+        await slack.send(msg)
